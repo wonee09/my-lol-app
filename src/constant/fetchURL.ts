cacc88f2f94b13597964b411d0e5d7ceee68965e
lee kiri <mango.man.e.ya@gmail.com>
2025-03-11 20:44:44 +0900

    feat : 아이템, 챔피언, 로테이션 API URL 상수 추가

---
+++ src/constant/fetchURL.ts
@@ -0,0 +1,7 @@
+export const ITEMLIST_URL =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json";
+
+export const CHAMPIONLIST_URL =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json";
+
+export const ROTATION_URL = `https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.RIOT_API_KEY}`;
