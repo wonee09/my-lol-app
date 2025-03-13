b7a24381dfd128e54da178590255eb706aa2f8f9
lee kiri <mango.man.e.ya@gmail.com>
2025-03-13 22:13:25 +0900

    feat : 챔피언 상세 정보를 위한 타입 정의 추가

---
+++ src/types/ChampionDetail.ts
@@ -0,0 +1,69 @@
+interface Image {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  }
+  
+  interface Stats {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  }
+  
+  interface Spell {
+    name: string;
+    description: string;
+    image: Image;
+  }
+  
+  interface Passive {
+    name: string;
+    description: string;
+    image: Image;
+  }
+  
+  export interface ChampionDetail {
+    id: string;
+    key: string;
+    name: string;
+    title: string;
+    image: Image;
+    skins: object[];
+    lore: string;
+    blurb: string;
+    allytips: string[];
+    enemytips: string[];
+    tags: string[];
+    partype: string;
+    info: {
+      attack: number;
+      defense: number;
+      magic: number;
+      difficulty: number;
+    };
+    stats: Stats;
+    spells: Spell[];
+    passive: Passive;
+    recommended: object[];
+  }
