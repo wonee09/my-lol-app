b2259a6b734d08e31076d3cfd120e7092de17d4b
lee kiri <mango.man.e.ya@gmail.com>
2025-03-15 23:06:42 +0900

    feat : 아이템 페이지에 제목 추가 및 레이아웃 수정

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -8,11 +8,11 @@ async function ItemPage() {
 
   // console.log("itemList", itemList);
   return (
-    <div className="mt-12 mb-12">
+    <div>
+      <h2 className="text-2xl text-center pb-6">아이템 목록</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
       {itemList.map((item) => {
         return (
-
           <div key={item.id} className="flex flex-col items-center justify-start rounded-lg py-2 border-neutral-200 gap-2 border-2 hover:border-primary">
             <Image
               src={`${DDRAGON_URL}${ver}/img/item/${item.image.full}`}
