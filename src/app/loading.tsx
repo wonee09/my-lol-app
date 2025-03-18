917267d450ee742c6e61563f932736879e7973a3
lee kiri <mango.man.e.ya@gmail.com>
2025-03-18 20:58:22 +0900

    feat : 로딩 컴포넌트에 전체 화면 중앙 정렬 스타일 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Loading = () => {
   return (
+    <div className="w-screen h-screen flex justify-center items-center">
+
     <span className="loader"></span>
+    </div>
   )
 }
 
