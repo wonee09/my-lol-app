988e7ed9530ea99a4f3160394de404ba4cb60dbd
Wonjang Choi <wonjangchoi@Wonjangui-Macmini.local>
2025-03-19 11:48:51 +0900

    d

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -2,7 +2,7 @@ import DetailStatsComp from "@/components/DetailStatsComp";
 import { DDRAGON_URL } from "@/constant/fetchURL";
 import { fetchChampionDetail } from "@/utils/serverApi";
 import Image from "next/image";
-
+//
 type Props = {
   params: {
     id: string;
@@ -16,18 +16,18 @@ const ChampionDetailPage = async ({ params }: Props) => {
     <div className="flex flex-col gap-2 mx-4 py-24">
       <h2 className="text-4xl font-bold">{championData.name}</h2>
       <h3 className="text-xl">{championData.title}</h3>
-        <div className="bg-neutral-500 h-96 relative">
-          <Image
-            src={`${DDRAGON_URL}img/champion/splash/${championData.id}_0.jpg`}
-            alt={championData.image.full}
-            quality={100}
-            fill
-            style={{
-              objectFit: "cover",
-              objectPosition: "top",
-            }}
-          />
-        </div>
+      <div className="bg-neutral-500 h-96 relative">
+        <Image
+          src={`${DDRAGON_URL}img/champion/splash/${championData.id}_0.jpg`}
+          alt={championData.image.full}
+          quality={100}
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "top",
+          }}
+        />
+      </div>
       <div className="mt-4 text-lg">{championData.blurb}</div>
 
       <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] mt-4 md:grid-cols-2 gap-8">
